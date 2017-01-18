7df72e10da0215a9cb4f82114593e3fa3c0abdc2
dsteplight <dsteplight@gmail.com>
2017-01-18 12:13:46 -0500

    initializing Angularfirebase with configs

--- bjj-tournament-demo/src/app/app.module.ts
+++ bjj-tournament-demo/src/app/app.module.ts
@@ -2,9 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { OneTournamentService } from './one-tournament.service';
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyBWr7gdIZN0jmS50cyGuKs-erbk69gyfio",
+   authDomain: "torneio-bjj.firebaseapp.com",
+   databaseURL: "https://torneio-bjj.firebaseio.com",
+   storageBucket: "torneio-bjj.appspot.com",
+   messagingSenderId: "638300069846"
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +22,8 @@ import { OneTournamentService } from './one-tournament.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [{provide: 'onetournament', useClass:OneTournamentService}],
   bootstrap: [AppComponent]
