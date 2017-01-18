416a31cd0ac41093590b775b653bb8ed27ffb76e
dsteplight <dsteplight@gmail.com>
2017-01-18 10:47:35 -0500

    testing out using anglefirebase to grab data

--- bjj-tournament-demo/src/app/app.component.ts
+++ bjj-tournament-demo/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component, Inject } from '@angular/core';
+import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
-  constructor(@Inject('onetournament') private onetournament){}
+  items: FirebaseObjectObservable<any>;
+  constructor(af: AngularFire, @Inject('onetournament') private onetournament){
+    this.items = af.database.object('/tournaments/naga-newark-2017/brackets/0/fighters');
+  }
 }
